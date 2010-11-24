98253ad52667e3f7e5e4f983b1023d5333574c02
panfu <panfu.gz@gmail.com>
2010-11-24 20:49:04 +0800

    fix :stringify_keys issue

--- lib/weibo/oauth_hack.rb
+++ lib/weibo/oauth_hack.rb
@@ -1,6 +1,17 @@
 # This hack is used for Weibo upload
 # as Sina Weibo doesn't follow the god damned OAuth specifications!!!
 
+unless {}.respond_to?(:stringify_keys)
+  class Hash
+     def stringify_keys
+       inject({}) do |options, (key, value)|
+         options[key.to_s] = value
+         options
+       end
+     end
+  end
+end
+
 module PostBodyHack
   @@parameters = nil
   def self.parameters=(val)
