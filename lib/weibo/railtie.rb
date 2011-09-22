f4c53150f7d1eb3514c2cf15cc9afa6b907e449d
Scott Ballantyne <ussballantyne@gmail.com>
2011-09-23 01:08:08 +0800

    making it not error out if the config/weibo.yml file doesnt exist

--- lib/weibo/railtie.rb
+++ lib/weibo/railtie.rb
@@ -1,9 +1,11 @@
 module Weibo
   class Railtie < Rails::Railtie
     config.after_initialize do
-      weibo_oauth = YAML.load_file(File.join(Rails.root.to_s, 'config', 'weibo.yml'))[Rails.env || "development"]
-      Weibo::Config.api_key = weibo_oauth["api_key"]
-      Weibo::Config.api_secret = weibo_oauth["api_secret"]
+      if File.exists?('config/weibo.yml')
+        weibo_oauth = YAML.load_file(File.join(Rails.root.to_s, 'config', 'weibo.yml'))[Rails.env || "development"]
+        Weibo::Config.api_key = weibo_oauth["api_key"]
+        Weibo::Config.api_secret = weibo_oauth["api_secret"]
+      end
     end
     rake_tasks do
       load "weibo/tasks/weibo.rake"
