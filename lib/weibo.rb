de12e5de517c01ea05bfee683fca459eaea16ceb
Scott Ballantyne <ussballantyne@gmail.com>
2011-09-23 01:38:34 +0800

    adding a message telling people about the weibo.yml

--- lib/weibo.rb
+++ lib/weibo.rb
@@ -61,7 +61,10 @@ if File.exists?('config/weibo.yml')
   Weibo::Config.api_key = weibo_oauth["api_key"]
   Weibo::Config.api_secret = weibo_oauth["api_secret"]
 else 
-  puts "you haven't made a config/weibo.yml file.  you should.  the weibo gem will work much better if you do"
+  puts "\n\n=========================================================\n\n" +
+       "  You haven't made a config/weibo.yml file.\n\n  You should.  \n\n  The weibo gem will work much better if you do\n\n" + 
+       "  Please set Weibo::Config.api_key and \n  Weibo::Config.api_secret\n  somewhere in your initialization process\n\n" +
+       "=========================================================\n\n"
 end
 
 begin
